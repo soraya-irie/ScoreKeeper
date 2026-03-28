0d264351d8f4b8ce9259458436429a61d9bc8c99
soraya-ire <rujiangkongshi@gmail.com>
2026-03-28 22:24:11 +0900

    Complete Section1 Step3 - add functionality with swift testing

--- ScoreKeeper/Scoreboard.swift
+++ ScoreKeeper/Scoreboard.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 struct Scoreboard {
-
+    var players: [Player] = [
+        Player(name: "Elisha", score: 0),
+        Player(name: "Andre", score: 0),
+        Player(name: "Jasmine", score: 0),
+    ]
 }
