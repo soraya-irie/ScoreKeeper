f8df4a165ce5c591ef6fa7ddca259b05286b7078
soraya-ire <rujiangkongshi@gmail.com>
2026-03-28 18:08:11 +0900

    Complete Section1 Step4

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [String] = ["Elisha", "Andre", "Jasmine"]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
