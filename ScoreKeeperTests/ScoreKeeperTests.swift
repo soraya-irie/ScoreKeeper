0098d3339a71fee7c6cabd01d9228cebb82167a0
soraya-ire <rujiangkongshi@gmail.com>
2026-03-29 15:46:11 +0900

    Complet Section3(just create ScoreKeeperTests) - add functionality with swift testing

---
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by soraya irie on 2026/03/29.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
