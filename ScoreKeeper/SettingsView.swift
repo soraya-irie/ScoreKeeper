a88d46bc322aa074146fb3bafd8d35aca2e80686
soraya-ire <rujiangkongshi@gmail.com>
2026-03-31 19:49:31 +0900

    Complete Section5 Step1 - add functionality with swift testing

---
+++ ScoreKeeper/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ScoreKeeper
+//
+//  Created by soraya irie on 2026/03/31.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
