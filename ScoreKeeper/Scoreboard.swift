5f0a768dec5c3d85e0b0e1be2e5b9d6608da2d69
soraya-ire <rujiangkongshi@gmail.com>
2026-03-31 19:37:11 +0900

    Complete Section4 Step13 - add functionality with swift testing

--- ScoreKeeper/Scoreboard.swift
+++ ScoreKeeper/Scoreboard.swift
@@ -18,7 +18,7 @@ struct Scoreboard {
 
     mutating func resetScores(to newValue: Int) {
         for index in 0..<players.count {
-            players[index].score = 0
+            players[index].score = newValue
         }
     }
 }
