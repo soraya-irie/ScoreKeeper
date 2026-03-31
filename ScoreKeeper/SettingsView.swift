ec89c5b04e4b6e3c988a94c7d7bae5bcae94d4b0
soraya-ire <rujiangkongshi@gmail.com>
2026-03-31 20:06:19 +0900

    Complete Section5 Step2 - add functionality with swift testing

--- ScoreKeeper/SettingsView.swift
+++ ScoreKeeper/SettingsView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .padding()
     }
 }
 
