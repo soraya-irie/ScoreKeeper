3263fb40a9f784dca7966eb294a5f2ce9760f9fc
soraya-ire <rujiangkongshi@gmail.com>
2026-03-31 19:27:30 +0900

    Complete Section4 Step11 - add functionality with swift testing

--- ScoreKeeperTests/ScoreKeeperTests.swift
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -10,7 +10,7 @@ import Testing
 
 struct ScoreKeeperTests {
 
-    @Test("Reset player scores")
+    @Test("Reset player scores", arguments: [0, 10, 20])
     func resetScores(to newValue: Int) async throws {
         var scoreboard = Scoreboard(players: [
             Player(name: "Elisha", score: 0),
